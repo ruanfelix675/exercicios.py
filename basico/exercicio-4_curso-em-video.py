d6f6630b8de577d9ac0f05bb6dbb02f764bba46c
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-03-31 16:07:29 -0300

    Create exercicio-4_curso-em-video.py

---
+++ basico/exercicio-4_curso-em-video.py
@@ -0,0 +1,14 @@
+'''
+Exercício Python 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+'''
+
+
+a = input('digite ')
+print(type(a))
+print(a.isalpha)
+print(a.isnumeric)
+print(a.isspace)
+print(a.isalnum)
+print(a.isupper)
+print(a.islower)
+print(a.istitle)
