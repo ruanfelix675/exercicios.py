622d59416513ceae1bb63dbb586c098fe608b30f
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-01 14:24:56 -0300

    Update and rename digite_um_numero_a_qual_deseja_saber_a_tabuada.py to digite_um_numero_e_veja_sua_tabuada.py

--- for/digite_um_numero_a_qual_deseja_saber_a_tabuada.py
+++
@@ -1,3 +0,0 @@
-n = int(input(' digite um numero a qual deseja saber a tabuada '))
-for control in range (1,11):
-    print(control*n)
---
+++ for/digite_um_numero_e_veja_sua_tabuada.py
@@ -0,0 +1,3 @@
+tabuada = int(input('digite um numero e veja a sua tabuada '))
+for i in range(1,11):
+    print(f'{tabuada} x {i} = {tabuada*i}  ')
