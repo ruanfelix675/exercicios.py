790e9699db7e6b0dc5b7ad12acd687af0fd2b49d
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 14:23:48 -0300

    Create fila de atendimento prioritário.py

---
+++ lista/fila de atendimento prioritário.py
@@ -0,0 +1,9 @@
+'''1.
+Uma fila de atendimento prioritário precisa colocar um idoso na
+primeira posição da lista.
+Dada a lista:
+fila = ["João", "Maria", "Carlos"]
+Coloque "Dona Ana" no início da fila e exiba o resultado.'''
+fila = ["João", "Maria", "Carlos"]
+fila.insert(0,"Dona Ana")
+print(fila)
