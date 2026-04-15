0c596ddea63b321615cd9c3e2b955816a808db3e
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 14:37:22 -0300

    Create Uma lista de alunos possui um cadastro incorreto na posição 1. Remova esse elemento.py

---
+++ listas/Uma lista de alunos possui um cadastro incorreto na posição 1. Remova esse elemento.py
@@ -0,0 +1,6 @@
+'''Uma lista de alunos possui um cadastro incorreto na posição 1.
+Remova esse elemento:
+alunos = ["Bruno", "Erro", "Carla", "Diego"]'''
+alunos = ["Bruno", "Erro", "Carla", "Diego"]
+alunos.remove("Erro")
+print(alunos)
