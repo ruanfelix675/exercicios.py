faab2118a5a19835ab4936be53d8747b54652d64
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 14:32:31 -0300

    Create Uma lista representa produtos em um carrinho.py

---
+++ listas/Uma lista representa produtos em um carrinho.py
@@ -0,0 +1,9 @@
+'''3.
+Uma lista representa produtos em um carrinho:
+carrinho = ["Arroz", "Feijão", "Macarrão", "Leite"]
+Remova o produto da posição 2 e mostre:
+• o item removido
+• a lista atualizada'''
+carrinho = ["Arroz", "Feijão", "Macarrão", "Leite"]
+carrinho.pop(2)
+print(carrinho)
