aa094f44798ccc0ed5c3d0674d96b6d6115f36b0
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 14:40:03 -0300

    Create ordem crescente e decrescente.py

---
+++ listas/ordem crescente e decrescente.py
@@ -0,0 +1,10 @@
+'''6.
+Uma loja deseja organizar os preços dos produtos:
+precos = [50, 10, 30, 20]
+• Organize em ordem crescente
+• Depois organize em ordem decrescente'''
+precos = [50, 10, 30, 20]
+precos.sort()
+print(precos)
+precos.sort(reverse=True)
+print(precos)
