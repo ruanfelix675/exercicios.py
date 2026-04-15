c9caeaa36923b45479fff44c8178e08ce6643ce9
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-15 15:29:42 -0300

    Update calculo media.py

--- basico/calculo media.py
+++ basico/calculo media.py
@@ -1,5 +1,5 @@
 n1 = float(input('sua nota' ))
-n2 = float(input('sua segunda hota' ))
+n2 = float(input('sua segunda nota' ))
 print(f'sua primeira nota foi: {n1}')
 print(f'sua primeira nota foi: {n2}')
 soma = (n1 + n2)/2
