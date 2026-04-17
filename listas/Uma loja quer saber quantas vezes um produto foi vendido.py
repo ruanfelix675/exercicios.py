012a519f71b80335ae95acc85f0d98b20ff88e52
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-17 12:43:38 -0300

    Create Uma loja quer saber quantas vezes um produto foi vendido.py

---
+++ listas/Uma loja quer saber quantas vezes um produto foi vendido.py
@@ -0,0 +1,5 @@
+'''Uma loja quer saber quantas vezes um produto foi vendido:
+vendas = ["camisa", "calça", "camisa", "sapato", "camisa"]
+Conte quantas vezes "camisa" aparece.'''
+vendas = ["camisa", "calça", "camisa", "sapato", "camisa"]
+print(vendas.count("camisa"))
