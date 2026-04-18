6458acfbbfb153f97fa098148151fc248bdea2bd
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-18 14:58:41 -0300

    Create texto-adaptavel-exercicio2.py

---
+++ funçao/texto-adaptavel-exercicio2.py
@@ -0,0 +1,7 @@
+'''Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+'''
+def Escreva (palavra):
+    print('='*len(palavra))
+    print(palavra)
+    print('='*len(palavra))
+Escreva('maioooonese')
