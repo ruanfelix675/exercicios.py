e5c97e35c70c4f77d46d7779b7d016aad47ffdc4
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-18 14:59:37 -0300

    Delete funçao directory

--- funçao/texto-adaptavel-exercicio2.py
+++
@@ -1,7 +0,0 @@
-'''Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
-'''
-def Escreva (palavra):
-    print('='*len(palavra))
-    print(palavra)
-    print('='*len(palavra))
-Escreva('maioooonese')
