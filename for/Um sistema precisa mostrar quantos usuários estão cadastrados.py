7629f80c442e76189b99cab17552fbcff3c6172e
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-19 16:11:25 -0300

    Create Um sistema precisa mostrar quantos usuários estão cadastrados.py

---
+++ for/Um sistema precisa mostrar quantos usuários estão cadastrados.py
@@ -0,0 +1,8 @@
+'''Um sistema precisa mostrar quantos usuários estão cadastrados:
+usuarios = ["Ana", "Bruno", "Carlos"]
+Mostre a quantidade de usuários.'''
+usuarios = ["Ana", "Bruno", "Carlos"]
+cont = 0
+for i in usuarios:
+    cont += 1
+    print(cont)
