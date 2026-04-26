e106b08a037ad28f050e83dcdb17c4f8a72e0a77
ruan_dev <ruanfeliz7777@gmail.com>
2026-04-26 13:14:47 -0300

    Delete while/validacao_sexo.py

--- while/validacao_sexo.py
+++
@@ -1,4 +0,0 @@
-n1 = n1 = str(input('digite seu sexo')).strip().upper()[0]
-while n1 not in 'MF' :
-    n1 = str(input('dados invalidos digite seu sexo novamente'))
-print('sexo registrado com suceso:',n1)
