43f0dacf45b6792a0e93a0ab0212e0cf439523be
ruan_dev <ruanfeliz7777@gmail.com>
2026-04-26 12:53:17 -0300

    Delete basico/exercicio 5 curso em video.py

--- basico/exercicio 5 curso em video.py
+++
@@ -1,4 +0,0 @@
-'''Exercício Python 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.'''
-
-n = int(input('digite um numero:'))
-print('numero digitado {} o antecesor {} e o sucesor e {}'.format(n,n-1,n+1))
