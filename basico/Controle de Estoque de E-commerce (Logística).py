47d38d31a4a44eb26267169f8a349e790273ec26
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-04-28 13:15:13 -0300

    Create Controle de Estoque de E-commerce (Logística).py

---
+++ basico/Controle de Estoque de E-commerce (Logística).py
@@ -0,0 +1,7 @@
+'''Exercício 2: Controle de Estoque de E-commerce (Logística)
+Cenário: Um e-commerce começou o dia com 250 unidades de um smartphone no
+estoque. Durante o dia, foram vendidos 78 unidades e chegaram mais 100 unidades de um
+fornecedor. Objetivo: Atualize a variável de estoque e exiba o saldo final.'''
+
+estoque = 250
+print(f'saldo final.R$:{(estoque-78)+100}')
