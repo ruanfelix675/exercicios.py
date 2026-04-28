25645bdab5c24ac88a69b543278f4844b046ec4d
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-04-28 13:10:25 -0300

    Create Cálculo de Bônus de Vendas (RHVendas).py

---
+++ basico/Cálculo de Bônus de Vendas (RHVendas).py
@@ -0,0 +1,12 @@
+'''Exercício 1: Cálculo de Bônus de Vendas (RH/Vendas)
+Cenário: Uma empresa decidiu dar um bônus de 10% sobre o faturamento total para a
+equipe de vendas. Objetivo: Calcule o valor do bônus e o faturamento final da empresa
+após subtrair esse bônus.
+● Faturamento inicial: 50.000
+● Percentual de bônus: 0.10'''
+Faturamento_inicial =  50000
+Percentual_de_bônus = 0.10
+bônus = Percentual_de_bônus*Faturamento_inicial
+faturamento_final = Faturamento_inicial - bônus
+print(faturamento_final)
+print(bônus)
