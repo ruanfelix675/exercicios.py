42628f089f35d692e6bcb3d760732ba46ec15e5c
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-04-29 13:08:30 -0300

    Create preço pão.py

---
+++ condicionais/preço pão.py
@@ -0,0 +1,6 @@
+preço_pao = 5
+dinheiro = float(input('digite a grana que voce tem:'))
+if dinheiro >= preço_pao:
+    print('parabens voce pode comprar o pao')
+else:
+    print('infelismente vc n pode comprar ;-;')
