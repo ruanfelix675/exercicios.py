f9a0ea7e0ce13706a516b148dab281b5d2c5e963
ruanfelixdev <ruanfeliz7777@gmail.com>
2026-05-04 15:41:52 -0300

    Create Consumo de gasolina.py

---
+++ basico/Consumo de gasolina.py
@@ -0,0 +1,8 @@
+# Consumo de gasolina
+# Carro faz 10 km por litro
+# Você quer andar X km
+# Quantos litros precisa?
+
+distancia = float(input('Digite o km que quer andar: '))
+litros = distancia / 10
+print(f'Você precisa de {litros:.2f} litros de gasolina')
